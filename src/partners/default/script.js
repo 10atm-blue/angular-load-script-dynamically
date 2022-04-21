7084a988b032cff80cee86431912990a1e7c63a1
Josue Lebon <josue@10atm.com>
2022-04-21 09:57:03 +0200

    Cleaning IDs

--- src/partners/default/script.js
+++ src/partners/default/script.js
@@ -1,10 +1,9 @@
 //alert("Hello you");
 
 function loadScript(url) {
-  // let url = "../../partners/mediamarkt/script.js";
   let script = document.createElement("script");
   script.src = url;
-  script.id = "partner-script";
+  //script.id = "partner-script";
   script.type = "text/javascript";
   script.async = true;
   script.charset = "utf-8";
@@ -12,9 +11,8 @@ function loadScript(url) {
 }
 
 function loadStyle(url) {
-  //  let url = "../../partners/mediamarkt/style.css";
   let style = this.document.createElement("link");
-  style.id = "partner-style";
+  //style.id = "partner-style";
   style.rel = "stylesheet";
   style.href = url;
   document.getElementsByTagName("head")[0].appendChild(style);
@@ -22,8 +20,6 @@ function loadStyle(url) {
 
 let partnerName = "mediamarkt";
 if (partnerName === "mediamarkt") {
-  // id="partner-script"
-  // id="partner-style"
   loadStyle("../../partners/" + partnerName + "/style.css");
   loadScript("../../partners/" + partnerName + "/script.js");
 }
